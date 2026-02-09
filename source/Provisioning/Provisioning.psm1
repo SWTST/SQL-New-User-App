45a54195daf04e0893ceb2a1aed09f110e3ad7cc
SWTST <Stevenwyatt02@hotmail.co.uk>
2026-02-09 02:20:03 +0000

    See last commit.

--- source/Provisioning/Provisioning.psm1
+++ source/Provisioning/Provisioning.psm1
@@ -1,27 +1,33 @@
 function Get-AllServers {
-    param (
-        [string]$SqlInstance
+    param(
+        [string]$SqlInstance = "DESKTOP-AD0K85U\MSSQL",
+        [string]$JsonPath = ".\config\servers.json",
+        [int]$Mode = 1
     )
 
-    if ((Get-DbaRegServer -SqlInstance $SqlInstance) -le 0) {
-        $AllServers = Get-DbaRegServer -SqlInstance $SqlInstance
-        return $AllServers 
-    }
-
-    else {
-        $AllServers = Get-Content .\config\servers.json
-        Return $AllServers
-    }
-    
+        if ($mode -eq 1) {
+        $servers = Get-DbaRegServer -SqlInstance $SqlInstance -ErrorAction Stop
+        return $servers
+        }
+        if ($mode -eq 2) {
+        return (Get-Content $JsonPath -Raw | ConvertFrom-Json)
+        }
 }
 
-Get-AllServers
 
-function Get-AllServerRoles {
-    param (
-    )
-    
+function Get-ServerRoles {
+        param(
+            [string]$Server
+        )
+
+    $roles = Get-DbaServerRole -SqlInstance $Server
+    Return $roles
+
 }
+
+# Getting top 1 server roles - Tweak logic for app
+$Server1 = Get-AllServers | Select-object -First 1
+Get-ServerRoles -Server $Server1.ServerName
 
 function Get-AllServerLogins {
     param (
